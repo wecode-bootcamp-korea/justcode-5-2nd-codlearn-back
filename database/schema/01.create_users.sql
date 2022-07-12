3ca39cfbf86376ba618253f34f201b59c41b8c67
Bokyung Kim <kbkllmlk17@gmail.com>
2022-07-12 21:56:28 +0900

    default profile photo updated

--- database/schema/01.create_users.sql
+++ database/schema/01.create_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
   user_name VARCHAR(50),
 	email VARCHAR(100) UNIQUE NOT NULL,
 	password VARCHAR(300) NOT NULL,
-	user_img VARCHAR(3000),
+	user_img VARCHAR(3000) DEFAULT "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdz6QQYO7SjHPl-ruRNK-KbfAKhjQEeOAmg&usqp=CAU",
   created_at DATETIME DEFAULT NOW(),
   updated_at DATETIME,
   PRIMARY KEY (id)
