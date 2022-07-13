295b56e9da7759ac1092eb0a2a269cde4cdbb4d1
Bokyung Kim <kbkllmlk17@gmail.com>
2022-07-13 16:51:08 +0900

    default null 수정

--- database/schema/01.create_users.sql
+++ database/schema/01.create_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
   user_name VARCHAR(50),
 	email VARCHAR(100) UNIQUE NOT NULL,
   social boolean default true,
-	password varchar(500)  default null,
+	password varchar(500) DEFAULT null,
 	user_img VARCHAR(3000) DEFAULT "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtdz6QQYO7SjHPl-ruRNK-KbfAKhjQEeOAmg&usqp=CAU",
   created_at DATETIME DEFAULT NOW(),
   updated_at DATETIME,
