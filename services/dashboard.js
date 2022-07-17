fee545e41f650dfa35bbe3dc84188bcc45633a62
Jieun Kim <jieun.k@hotmail.com>
2022-07-16 19:34:16 -0700

    Add: wishlist

--- services/dashboard.js
+++ services/dashboard.js
@@ -1,12 +1,43 @@
-const { checkMyClassItems, getItems } = require('../models/dashboard');
+const {
+  checkTableNotEmpty,
+  getUser,
+  getCoursesBySort,
+  getWishListItems,
+} = require('../models/dashboard');
 
-const doesExist = async (userId) => {
-  return await checkMyClassItems(userId);
+const limit = 3;
+
+const isMyCourseNotEmpty = async (userId) => {
+  return await checkTableNotEmpty(userId, 'my_classes');
+};
+
+const isWishListNotEmpty = async (userId) => {
+  return await checkTableNotEmpty(userId, 'wishlist');
 };
 
 const getDashBoardItems = async (userId) => {
-  const items = await getItems(userId);
-  return items;
+  let user = await getUser(userId);
+  let recentlyRegistered;
+  let recentlyTaken;
+  let wishlist;
+  if (isMyCourseNotEmpty) {
+    recentlyRegistered = {
+      recentlyRegistered: await getCoursesBySort(userId, 'created_at', limit),
+    };
+    recentlyTaken = {
+      recentlyTaken: await getCoursesBySort(userId, 'progress', limit),
+    };
+  }
+  if (isWishListNotEmpty) {
+    wishlist = { wishlist: await getWishListItems(userId, limit) };
+  }
+  const result = Object.assign(
+    ...user,
+    recentlyRegistered,
+    recentlyTaken,
+    wishlist
+  );
+  return result;
 };
 
 module.exports = { getDashBoardItems };
