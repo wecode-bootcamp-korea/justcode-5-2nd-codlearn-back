5e094a5e85299997fed8efbde9ff600e8fe59579
Bokyung Kim <kbkllmlk17@gmail.com>
2022-07-18 11:01:01 +0900

    conflict 수정 + detail page without review

---
+++ routes/coures.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const asyncWrap = require('../async-wrap');
+const router = Router();
+
+const {
+    readCourseDetail
+  } = require('../controller/course');
+
+
+router.get('/:title',  asyncWrap(readCourseDetail));
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cartRouter = require('./cart');
 const myClassRouter = require('./myClasses');
 const coursesRouter = require('./courses');
+const courseRouter = require('./course');
 const dashBoardRouter = require('./dashboard');
 const wishlistRouter = require('./wishlist');
 
@@ -10,6 +11,7 @@ const router = express.Router();
 router.use('/cart', cartRouter);
 router.use('/my-classes', myClassRouter);
 router.use('/courses', coursesRouter);
+router.use('/course', coursesRouter);
 router.use('/dashboard', dashBoardRouter);
 router.use('/wishlist', wishlistRouter);
 
